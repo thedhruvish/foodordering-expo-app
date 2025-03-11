f53a8754a1516aa4c5f578d4f2177febc74dcef4
thedhruvish <thedhruvish@gmail.com>
2025-03-11 17:44:27 +0530

    add the shoping cart

--- app/(tabs)/menu/[id].tsx
+++ app/(tabs)/menu/[id].tsx
@@ -1,19 +1,27 @@
 import { View, Text, Image, StyleSheet, Pressable } from "react-native";
 import React, { useState } from "react";
-import { Stack, useLocalSearchParams } from "expo-router";
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
 import products from "@/assets/data/products";
 import { defaultPizzaImage } from "@/components/ProductListItem";
 import Button from "@/components/Button";
+import { useCart } from "@/providers/CartProvider";
+import { PizzaSize } from "@/types";
 
-const size = ["S", "M", "L", "XL"];
+const size: PizzaSize[] = ["S", "M", "L", "XL"];
 
 const ProductDetailScreen = () => {
   const { id } = useLocalSearchParams();
-  const [selectedSize, setSelectedSize] = useState("M");
+  const [selectedSize, setSelectedSize] = useState<PizzaSize>("M");
+  const { addItem } = useCart();
   const product = products.find((p) => p.id.toString() === id);
+  const router = useRouter();
 
   const addToCart = () => {
-    console.warn("Adding to cart, size " + selectedSize);
+    if (!product) {
+      return;
+    }
+    addItem(product, selectedSize);
+    router.push("/cart");
   };
 
   if (!product) {
---
+++ providers/CartProvider.tsx
@@ -0,0 +1,62 @@
+import { CartItem, Product } from "@/types";
+import { randomUUID } from "expo-crypto";
+import { createContext, PropsWithChildren, useContext, useState } from "react";
+type CartType = {
+  items: CartItem[];
+  addItem: (product: Product, size: CartItem["size"]) => void;
+  updateQuantity: (itemId: string, amount: -1 | 1) => void;
+};
+export const CartContext = createContext<CartType>({
+  items: [],
+  addItem: () => {},
+  updateQuantity: () => {},
+});
+
+const CartProvider = ({ children }: PropsWithChildren) => {
+  const [items, setItems] = useState<CartItem[]>([]);
+
+  const addItem = (product: Product, size: CartItem["size"]) => {
+    console.log(product)
+    const existingItem = items.find(
+      (item) => item.product === product && item.size === size
+    );
+
+    if (existingItem) {
+      updateQuantity(existingItem.id, 1);
+      return;
+    }
+
+    const newCartItem: CartItem = {
+      id: randomUUID(),
+      product,
+      product_id: product.id,
+      size,
+      quantity: 1,
+    };
+    setItems([newCartItem, ...items]);
+  };
+  const updateQuantity = (itemId: string, amount: -1 | 1) => {
+    setItems(
+      items
+        .map((items) =>
+          items.id !== itemId
+            ? items
+            : {
+                ...items,
+                quantity: items.quantity + amount,
+              }
+        )
+        .filter((item) => item.quantity > 0)
+    );
+  };
+
+  return (
+    <CartContext.Provider value={{ items, addItem, updateQuantity }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
+
+export const useCart = () => useContext(CartContext);
