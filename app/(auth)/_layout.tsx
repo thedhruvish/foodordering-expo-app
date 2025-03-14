b3570422fae9c104c685098cc435c93cc085d12b
thedhruvish <thedhruvish@gmail.com>
2025-03-14 12:48:01 +0530

    protect the routers

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,6 +1,11 @@
-import { Stack } from "expo-router";
+import { useAuth } from "@/providers/AuthProvider";
+import { Redirect, Stack } from "expo-router";
 import React from "react";
 
 export default function AuthLayout() {
+  const { session } = useAuth();
+  if (session) {
+    return <Redirect href={"/"} />;
+  }
   return <Stack />;
 }
--- providers/AuthProvider.tsx
+++ providers/AuthProvider.tsx
@@ -10,21 +10,37 @@ import {
 
 type AuthData = {
   session: Session | null;
+  profile: any;
   loading: Boolean;
+  isAdmin: Boolean;
 };
 
 const AuthContext = createContext<AuthData>({
   session: null,
   loading: true,
+  profile: null,
+  isAdmin: false,
 });
 
 export default function AuthProvider({ children }: PropsWithChildren) {
   const [session, setSession] = useState<Session | null>(null);
+  const [profile, setProfile] = useState(null);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchSession = async () => {
-      const { data } = await supabase.auth.getSession();
-      setSession(data.session);
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
+      setSession(session);
+      if (session) {
+        const { data } = await supabase
+          .from("profiles")
+          .select("*")
+          .eq("id", session.user.id)
+          .single();
+        setProfile(data || null);
+      }
+      setLoading(false);
     };
     fetchSession();
     supabase.auth.onAuthStateChange((_event, session) => {
@@ -33,7 +49,9 @@ export default function AuthProvider({ children }: PropsWithChildren) {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ session, loading }}>
+    <AuthContext.Provider
+      value={{ session, loading, profile, isAdmin: profile?.group === "ADMIN" }}
+    >
       {children}
     </AuthContext.Provider>
   );
