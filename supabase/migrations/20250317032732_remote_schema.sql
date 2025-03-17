3a42abffbf31beb8e547516fc552587d13a3792d
thedhruvish <thedhruvish@gmail.com>
2025-03-17 09:18:39 +0530

    add the trigger action in db

--- supabase/migrations/20250317032732_remote_schema.sql
+++ supabase/migrations/20250317032732_remote_schema.sql
@@ -76,6 +76,11 @@ begin
 end;
 $$;
 
+-- trigger the function every time a user is created
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
+
 
 ALTER FUNCTION "public"."handle_new_user"() OWNER TO "postgres";
 
